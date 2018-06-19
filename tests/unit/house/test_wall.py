6d470b5e5f0fa01b28c36e411314986e3abfb1cf
Leonardo Antunes <antunesleo4@gmail.com>
2018-06-18 21:32:24 -0300

    testing create with instance and create new

--- tests/unit/house/test_wall.py
+++ tests/unit/house/test_wall.py
@@ -19,3 +19,18 @@ class NoteTest(base.TestCase):
         one_or_none_mock.return_value = None
         with self.assertRaises(wall.Note.NotFound):
             wall.Note.create_with_id(1)
+
+    @base.TestCase.mock.patch('app.house.wall.Note.repository')
+    def test_create_with_instance(self, repository_mock):
+        repository_mock = self.mock.MagicMock()
+        instance_mock = self.mock.MagicMock()
+        created_note = wall.Note.create_with_instance(instance_mock)
+        self.assertTrue(isinstance(created_note, wall.Note))
+
+    @base.TestCase.mock.patch('app.house.wall.Note.repository.create_from_json')
+    @base.TestCase.mock.patch('app.house.wall.Note.repository')
+    def test_create_new(self, repository_mock, create_from_json_mock):
+        repository_mock = self.mock.MagicMock()
+        instance_mock = self.mock.MagicMock()
+        created_note = wall.Note.create_new(instance_mock)
+        self.assertTrue(create_from_json_mock.called)
