ac254cd318fa0cc423c3141fd6fb3a75d4d27861
Leonardo Antunes <antunesleo4@gmail.com>
2018-07-08 10:38:18 -0300

    Removing unused stuff

--- tests/base.py
+++ tests/base.py
@@ -1,11 +1,6 @@
 import unittest, os
 from mock import mock
 
-os.environ.update({
-    'APP_SETTINGS': 'app.config.TestingConfig',
-    'DATABASE_URL': 'postgresql+psycopg2://manotes:manotes@localhost/manotes'
-})
-
 from app import initialize
 
 
