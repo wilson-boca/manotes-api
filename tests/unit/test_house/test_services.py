e13462a25ed7286f98ca228ac649a3aee0c29bb4
Leonardo Antunes <antunesleo4@gmail.com>
2018-12-10 21:33:31 -0200

    Testing security service

--- tests/unit/test_house/test_services.py
+++ tests/unit/test_house/test_services.py
@@ -31,3 +31,54 @@ class NoteServiceListForUserTest(base.TestCase):
         note_mock.list_for_user = self.mock.MagicMock(return_value=[])
         notes = services.NoteService.list_for_user(1)
         self.assertTrue(isinstance(notes, list))
+
+
+class SecurityServiceGenerateTokenTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.house.services.secrets.token_hex')
+    def test_should_call_secrets_to_token_hex(self, token_hex_mock):
+        token_hex_mock.return_value = 'testyjednostkowe'
+        services.SecurityService.generate_a_token()
+        token_hex_mock.assert_called_with(40)
+
+    @base.TestCase.mock.patch('app.house.services.secrets.token_hex')
+    def test_should_return_token(self, token_hex_mock):
+        token_hex_mock.return_value = 'testyjednostkowe'
+        token = services.SecurityService.generate_a_token()
+        self.assertEqual(token, 'testyjednostkowe')
+
+
+class SecurityServiceHashTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.house.services.pbkdf2_sha256.hash')
+    def test_should_call_pbfkd2_sha256_to_hash(self, hash_mock):
+        hash_mock.return_value = 'testyjednostkowe'
+        services.SecurityService.hash('slowo')
+        hash_mock.assert_called_with('slowo')
+
+    @base.TestCase.mock.patch('app.house.services.pbkdf2_sha256.hash')
+    def test_should_return_hash(self, hash_mock):
+        hash_mock.return_value = 'testyjednostkowe'
+        haash = services.SecurityService.hash('slowo')
+        self.assertEqual(haash, 'testyjednostkowe')
+
+
+class SecurityServiceIsStringEqualsToHashTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.house.services.pbkdf2_sha256.verify')
+    def test_should_call_pbfkd2_sha256_to_verify(self, verify_mock):
+        verify_mock.return_value = True
+        services.SecurityService.is_string_equals_to_hash('slowo', 'testyjednostkowe')
+        verify_mock.assert_called_with('slowo', 'testyjednostkowe')
+
+    @base.TestCase.mock.patch('app.house.services.pbkdf2_sha256.verify')
+    def test_should_return_true_if_string_is_equals_to_hash(self, verify_mock):
+        verify_mock.return_value = True
+        is_equal = services.SecurityService.is_string_equals_to_hash('slowo', 'testyjednostkowe')
+        self.assertTrue(is_equal)
+
+    @base.TestCase.mock.patch('app.house.services.pbkdf2_sha256.verify')
+    def test_should_return_false_if_string_is_not_equals_to_hash(self, verify_mock):
+        verify_mock.return_value = False
+        is_equal = services.SecurityService.is_string_equals_to_hash('slowo', 'testyjednostkowe')
+        self.assertFalse(is_equal)
