629d972248caf75824973e964477c56bff6df113
Leonardo Antunes <antunesleo4@gmail.com>
2018-12-11 22:19:21 -0200

    Testing Account Resource

--- tests/unit/test_resource/test_resources.py
+++ tests/unit/test_resource/test_resources.py
@@ -4,6 +4,152 @@ from app import exceptions
 from app.resource import resources
 
 
+class AccountResourceGetTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_not_allowed(self, g_mock):
+        avatar_resource = resources.AccountResource()
+        response = avatar_resource.get()
+        self.assertEqual(json.loads(response.data), {'result': 'Method not allowed'})
+
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_status_code_405(self, g_mock):
+        avatar_resource = resources.AccountResource()
+        response = avatar_resource.get()
+        self.assertEqual(response.status_code, 405)
+
+
+class AccountResourceDeleteTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_not_allowed(self, g_mock):
+        avatar_resource = resources.AccountResource()
+        response = avatar_resource.delete()
+        self.assertEqual(json.loads(response.data), {'result': 'Method not allowed'})
+
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_status_code_405(self, g_mock):
+        avatar_resource = resources.AccountResource()
+        response = avatar_resource.delete()
+        self.assertEqual(response.status_code, 405)
+
+
+class AccountResourcePostTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.visitor_user')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_call_visitor_user_to_create_account(self, g_mock, payload_mock, visitor_user_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        visitor_user_mock.create_account.return_value = user_mock
+        account_resource = resources.AccountResource()
+        account_resource.post()
+        self.assertTrue(visitor_user_mock.create_account.called)
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.visitor_user')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_call_user_to_as_dict(self, g_mock, payload_mock, visitor_user_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        visitor_user_mock.create_account.return_value = user_mock
+        account_resource = resources.AccountResource()
+        account_resource.post()
+        self.assertTrue(user_mock.as_dict.called)
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.visitor_user')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_user(self, g_mock, payload_mock, visitor_user_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        visitor_user_mock.create_account.return_value = user_mock
+        account_resource = resources.AccountResource()
+        response = account_resource.post()
+        self.assertEqual(response, {"username": "antunesleo"})
+
+
+class AccountResourcePutTest(base.TestCase):
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.me')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_call_me_to_update_account(self, g_mock, payload_mock, me_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        me_mock.update.return_value = None
+        account_resource = resources.AccountResource()
+        account_resource.put()
+        self.assertTrue(me_mock.update.called)
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.me')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_call_me_to_as_dict(self, g_mock, payload_mock, me_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        me_mock.update.return_value = None
+        me_mock.as_dict.return_value = {"username": "antunesleo", "password": "fsfdsafdsa34"}
+        account_resource = resources.AccountResource()
+        account_resource.put()
+        self.assertTrue(me_mock.as_dict.called)
+
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.me')
+    @base.TestCase.mock.patch('app.resource.resources.AccountResource.payload')
+    @base.TestCase.mock.patch('app.resource.resources.g')
+    def test_should_return_me_as_dict(self, g_mock, payload_mock, me_mock):
+        g_mock.authenticated = True
+        payload_mock = {
+            "username": "antunesleo",
+            "password": 12345
+        }
+        user_mock = self.mock.MagicMock()
+        user_mock.as_dict.return_value = {
+            "username": "antunesleo"
+        }
+        me_mock.update.return_value = None
+        me_mock.as_dict.return_value = {"username": "antunesleo", "password": "fsfdsafdsa34"}
+        account_resource = resources.AccountResource()
+        response = account_resource.put()
+        self.assertEqual(response, {"username": "antunesleo", "password": "fsfdsafdsa34"})
+
+
 class NoteResourceGetTest(base.TestCase):
 
     @base.TestCase.mock.patch('app.resource.resources.g')
