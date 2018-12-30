e53b5fc5a2b3133ca985053453c7a93ee5720fd1
Leonardo Antunes <antunesleo4@gmail.com>
2018-12-30 10:22:16 -0200

    Tested postman

--- tests/unit/test_mail/test_postman.py
+++ tests/unit/test_mail/test_postman.py
@@ -1,31 +1,95 @@
+from app.mail import postman
 from tests import base
+
+from app import config as config_module
+
+config = config_module.get_config()
 
 
 class PostmanSendConfirmationEmailTest(base.TestCase):
 
-    def test_should_instantiate_mime_multipart(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_instantiate_mime_multipart(self, smtplib_mock, mime_multipart_mock):
+        postman.Postman.send_confirmation_email('breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        self.assertTrue(mime_multipart_mock.called)
 
-    def test_should_call_mime_text_with_body_and_plain(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_mime_text_with_body_and_plain(self, smtplib_mock, mime_multipart_mock, mime_text):
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        body = """
+            Hello, {0}.
+            This is a confirmation email.
+            Welcome to manotes
+            """.format('Breno')
+        mime_text.assert_called_with(body, 'plain')
 
-    def test_should_call_msg_to_attach_mime_text(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_msg_to_attach_mime_text(self, smtplib_mock, mime_multipart_mock, mime_text):
+        msg_mock = self.mock.MagicMock()
+        mime_multipart_mock.return_value = msg_mock
+        mime_text_instance_mock = self.mock.MagicMock
+        mime_text.return_value = mime_text_instance_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        msg_mock.attach.assert_called_with(mime_text_instance_mock)
 
-    def test_should_call_smtplib_to_instatiate_smtp(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_smtplib_to_instatiate_smtp(self, smtplib_mock, mime_multipart_mock, mime_text):
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        smtplib_mock.SMTP.assert_called_with(config.SMTP_HOST, config.SMTP_PORT)
 
-    def test_should_call_server_to_starttls(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_server_to_starttls(self, smtplib_mock, mime_multipart_mock, mime_text):
+        server_mock = self.mock.MagicMock()
+        smtplib_mock.SMTP.return_value = server_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        self.assertTrue(server_mock.starttls.called)
 
-    def test_should_call_server_to_login(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_server_to_login(self, smtplib_mock, mime_multipart_mock, mime_text):
+        server_mock = self.mock.MagicMock()
+        smtplib_mock.SMTP.return_value = server_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        server_mock.login.assert_called_with(config.SMTP_USERNAME, config.SMTP_PASSWORD)
 
-    def test_should_call_msg_to_as_string(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_msg_to_as_string(self, smtplib_mock, mime_multipart_mock, mime_text):
+        msg_mock = self.mock.MagicMock()
+        mime_multipart_mock.return_value = msg_mock
+        mime_text_instance_mock = self.mock.MagicMock
+        mime_text.return_value = mime_text_instance_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        self.assertTrue(msg_mock.as_string.called)
 
-    def test_should_call_server_to_sendmail(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_server_to_sendmail(self, smtplib_mock, mime_multipart_mock, mime_text):
+        msg_mock = self.mock.MagicMock()
+        msg_mock.as_string.return_value = 'AAA'
+        mime_multipart_mock.return_value = msg_mock
+        server_mock = self.mock.MagicMock()
+        smtplib_mock.SMTP.return_value = server_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        server_mock.sendmail.assert_called_with('breno@manotes.com', 'a@manotes.com', 'AAA')
 
-    def test_should_call_server_to_quit(self):
-        pass
+    @base.mock.patch('app.mail.postman.MIMEText')
+    @base.mock.patch('app.mail.postman.MIMEMultipart')
+    @base.mock.patch('app.mail.postman.smtplib')
+    def test_should_call_server_to_quit(self, smtplib_mock, mime_multipart_mock, mime_text):
+        server_mock = self.mock.MagicMock()
+        smtplib_mock.SMTP.return_value = server_mock
+        postman.Postman.send_confirmation_email('Breno', 'breno@manotes.com', 'a@manotes.com', 'How do you eim?')
+        self.assertTrue(server_mock.quit.called)
