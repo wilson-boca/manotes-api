631c287e416182a368050ad68df63182bd549d2e
Leonardo Antunes <antunesleo4@gmail.com>
2018-12-30 13:15:26 -0200

    testing authentication

--- tests/unit/test_authentication.py
+++ tests/unit/test_authentication.py
@@ -2,7 +2,7 @@ from tests import base
 from app import authentication, exceptions
 
 
-class AuthServiceAuthenticateWithCredentials(base.TestCase):
+class AuthServiceAuthenticateWithCredentialsTest(base.TestCase):
 
     @base.mock.patch('app.house.services.SecurityService.is_string_equals_to_hash', base.mock.MagicMock())
     @base.mock.patch('app.house.residents.User.create_with_username')
@@ -46,19 +46,40 @@ class AuthServiceAuthenticateWithCredentials(base.TestCase):
         self.assertEqual(user, user_mock)
 
 
-class AuthServiceAuthenticateWithToken(base.TestCase):
+class AuthServiceAuthenticateWithTokenTest(base.TestCase):
 
-    def test_should_call_create_with_token(self):
-        pass
+    @base.mock.patch('app.authentication.g', base.mock.MagicMock())
+    @base.mock.patch('app.house.residents.User.create_with_token')
+    def test_should_call_create_with_token(self, create_with_token_mock):
+        authentication.AuthService.authenticate_with_token('HAUHIAKJA')
+        create_with_token_mock.assert_called_with('HAUHIAKJA')
 
-    def test_should_set_user_on_g_if_not_found_not_raised(self):
-        pass
+    @base.mock.patch('app.authentication.g')
+    @base.mock.patch('app.house.residents.User.create_with_token')
+    def test_should_set_user_on_g_if_not_found_not_raised(self, create_with_token_mock, g_mock):
+        user_mock = self.mock.MagicMock(token='HAIUHIUAAK')
+        create_with_token_mock.return_value = user_mock
+        authentication.AuthService.authenticate_with_token('HAUHIAKJA')
+        self.assertEqual(user_mock, g_mock.user)
 
-    def test_should_set_current_token_on_if_not_found_not_raised(self):
-        pass
+    @base.mock.patch('app.authentication.g')
+    @base.mock.patch('app.house.residents.User.create_with_token')
+    def test_should_set_current_token_on_if_not_found_not_raised(self, create_with_token_mock, g_mock):
+        user_mock = self.mock.MagicMock(token='HAIUHIUAAK')
+        create_with_token_mock.return_value = user_mock
+        authentication.AuthService.authenticate_with_token('HAUHIAKJA')
+        self.assertEqual(user_mock.token, g_mock.current_token)
 
-    def test_should_set_authenticated_true_on_g_if_not_found_not_raised(self):
-        pass
+    @base.mock.patch('app.authentication.g', base.mock.MagicMock())
+    @base.mock.patch('app.house.residents.User.create_with_token')
+    def test_should_set_authenticated_true_on_g_if_not_found_not_raised(self, create_with_token_mock):
+        user_mock = self.mock.MagicMock(token='HAIUHIUAAK')
+        create_with_token_mock.return_value = user_mock
+        authentication.AuthService.authenticate_with_token('HAUHIAKJA')
+        self.assertTrue(user_mock.authenticated)
 
-    def test_should_set_authenticated_false_on_g_if_not_found_raised(self):
-        pass
+    @base.mock.patch('app.house.residents.User.create_with_token', base.mock.MagicMock(side_effect=exceptions.NotFound))
+    @base.mock.patch('app.authentication.g')
+    def test_should_set_authenticated_false_on_g_if_not_found_raised(self, g_mock):
+        authentication.AuthService.authenticate_with_token('HAUHIAKJA')
+        self.assertFalse(g_mock.authenticated)
