cf745856c8cf423eb138f4bbf586d898c4e43f1b
Wilson Ferreira <wilson.ferreira@inceres.com.br>
2019-04-14 10:34:50 -0300

    Updated version

---
+++ gunicorn_conf.py
@@ -0,0 +1,11 @@
+import os
+
+bind = '0.0.0.0:{}'.format(os.environ['PORTA'])
+user = os.environ.get('GUNICORN_USER')
+workers = int(os.environ.get('GUNICORN_WORKERS', 2))
+timeout = 600
+accesslog = '{}/access.log'.format(os.environ['LOG_DIR'])
+errorlog = '{}/error.log'.format(os.environ['LOG_DIR'])
+loglevel = 'debug'
+capture_output = True
+worker_class = 'gevent'
