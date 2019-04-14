2e9a515c7b2e1d5cc5cd3a302524f2d82d981547
Wilson Ferreira <wilson.ferreira@inceres.com.br>
2019-04-14 10:40:18 -0300

    Updated version

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,6 @@
 import os
 
 bind = '0.0.0.0:{}'.format(os.environ['PORT'])
-user = os.environ.get('GUNICORN_USER')
 workers = int(os.environ.get('GUNICORN_WORKERS', 2))
 timeout = 600
 accesslog = '{}/access.log'.format(os.environ['LOG_DIR'])
