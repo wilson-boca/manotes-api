31bc9912dec2611f8ae2ac3c052567d8c3cfad1d
Wilson Ferreira <wilson.ferreira@inceres.com.br>
2019-04-16 13:25:50 -0300

    fix

--- app/store/reception.py
+++ app/store/reception.py
@@ -14,7 +14,7 @@ class Clerk(object):
             raise exceptions.InvalidEmail('Could not create user account because the email: {} is invalid'.format(user['email']))
         created_user = services.UserService.create_new(user)
         name = created_user.username
-        from_address = "antunesleo4@gmail.com"
+        from_address = "wilson.boca@gmail.com"
         to_address = created_user.email
         subject = "Test"
         tasks.start_send_email(name, from_address, to_address, subject)
